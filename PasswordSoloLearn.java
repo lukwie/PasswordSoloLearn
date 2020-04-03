d6ce352e97262e91f26005a8597b5165ea5b599f
luwie <lukwie@gmail.com>
2020-04-03 22:36:23 +0200

    without checed statement

--- PasswordSoloLearn.java
+++ PasswordSoloLearn.java
@@ -6,7 +6,29 @@ public class PasswordSoloLearn {
 
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		System.out.println(input);
+		//System.out.println(input);
+		char [] in = input.toCharArray();
+		/*for (char i : in) {
+			System.out.println(i);
+		}*/
+		int letters = 0;
+		int numbers = 0;
+		int signs = 0;
+		int len = in.length;
 
+		System.out.println("len: " + len);
+
+		for (char i : in) {
+			if (i >= '0' && i <= '9') {
+				numbers++;
+			}
+			if (i>='a' && i<='z' || i>='A' && i<='Z') {
+				letters++;
+			}
+			if (i=='!' || i=='@' || i=='#' || i=='$' || i=='%' || i=='&' || i=='*') {
+				signs++;
+			}
+		}
+		System.out.println("letters: " + letters + " numbers: " + numbers + " signs: " + signs);
 	}
 }
